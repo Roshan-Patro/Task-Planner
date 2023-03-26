7c3dc4b3fd0900592a689a592b6a2917274c71cb
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2023-03-27 00:13:27 +0530

    assign-task-to-user feature added

--- TaskPlanner/src/main/java/com/paypal/controller/TaskController.java
+++ TaskPlanner/src/main/java/com/paypal/controller/TaskController.java
@@ -3,13 +3,16 @@ package com.paypal.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paypal.dto.CreateTaskDto;
 import com.paypal.model.Task;
+import com.paypal.model.User;
 import com.paypal.service.TaskService;
 
 @RestController
@@ -23,5 +26,11 @@ public class TaskController {
 	public ResponseEntity<Task> createTask(@RequestBody CreateTaskDto dto){
 		Task createdTask = tservice.createTask(dto);
 		return new ResponseEntity<Task>(createdTask, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/assigntouser/{taskId}/{userId}")
+	public ResponseEntity<User> assignTaskToUser(@PathVariable("taskId") Integer taskId, @PathVariable("userId") Integer userId){
+		User assignedUser = tservice.assignTaskToUser(taskId,userId);
+		return new ResponseEntity<User>(assignedUser, HttpStatus.OK);
 	}
 }
--- TaskPlanner/src/main/java/com/paypal/service/TaskService.java
+++ TaskPlanner/src/main/java/com/paypal/service/TaskService.java
@@ -4,7 +4,10 @@ import com.paypal.dto.CreateTaskDto;
 import com.paypal.exception.TaskException;
 import com.paypal.exception.UserException;
 import com.paypal.model.Task;
+import com.paypal.model.User;
 
 public interface TaskService {
 	public Task createTask(CreateTaskDto dto) throws TaskException, UserException;
+
+	public User assignTaskToUser(Integer taskId, Integer userId) throws TaskException, UserException;
 }
