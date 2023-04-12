aecf7324d8a5b72ef169d33981cf96f72f9b743f
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2023-04-12 15:39:13 +0530

    user-logout API's fetching function completed in frontend

--- TaskPlanner/src/main/java/com/paypal/config/AppConfigCors.java
+++ TaskPlanner/src/main/java/com/paypal/config/AppConfigCors.java
@@ -1,3 +1,27 @@
+//package com.paypal.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class AppConfigCors implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry
+//                .addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*")
+//                .exposedHeaders("Authorization")
+//                .allowCredentials(true)
+//                .maxAge(3600);
+//    }
+//}
+
+
+
 package com.paypal.config;
 
 import org.springframework.context.annotation.Configuration;
